b896b2124b8223e6a77348f583cf5afad2636261
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2018-02-28 12:42:27 +0900

    chapter 21

--- xunit.py
+++ xunit.py
@@ -1,13 +1,24 @@
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
+
 class TestCase:
     def __init__(self, name):
         self.name = name
     def setUp(self):
         pass
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
     def tearDown(self):
         pass
 
@@ -16,6 +27,8 @@ class WasRun(TestCase):
         self.log = self.log + "testMethod "
     def setUp(self):
         self.log = "setUp "
+    def testBrokenMethod(self):
+        raise Exception
     def tearDown(self):
         self.log = self.log + "tearDown "
 
@@ -24,5 +37,15 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown " == test.log)
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+#TestCaseTest("testFailedResult").run()
